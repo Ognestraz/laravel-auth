bfeb71bde5138c753d984fd1d7493a0cb23eedf1
ognestraz <ognestraz@gmail.com>
2016-01-24 11:46:24 +0300

    First prototype

---
+++ src/app/Http/Controllers/AuthController.php
@@ -0,0 +1,41 @@
+<?php namespace Ognestraz\Auth\Http\Controllers;
+
+use Illuminate\Foundation\Auth\ThrottlesLogins;
+use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+
+class AuthController extends \Illuminate\Routing\Controller
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthenticatesAndRegistersUsers, ThrottlesLogins;
+
+    /**
+     * Where to redirect users after login / registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/';
+    protected $redirectAfterLogout = '/login';
+
+    /**
+     * Create a new authentication controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest', ['except' => 'logout']);
+    }
+    
+    /**
+     * Show the application login form.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function showLoginForm()
+    {
+        return view('auth::login');
+    }
+}
---
+++ src/app/Providers/AuthServiceProvider.php
@@ -0,0 +1,41 @@
+<?php namespace Ognestraz\Auth\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if (!$this->app->routesAreCached()) {
+            require __DIR__.'/../Http/routes.php';
+        }
+        
+        $pathViews = __DIR__.'/../../resources/views';
+        $packageName = 'auth-admin';
+        $this->loadViewsFrom($pathViews, $packageName);
+        
+        $pathPublicCss = __DIR__.'/../../public/css/' . $packageName . '.css';
+        $pathUnitTest = __DIR__.'/../../tests';
+        
+        $this->publishes([
+            $pathViews => base_path('resources/views/vendor/' . $packageName),
+            $pathPublicCss => base_path('public/css/' . $packageName . '.css'),
+            $pathUnitTest => base_path('tests/vendor')
+        ]);
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
