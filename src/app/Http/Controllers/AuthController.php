5433235201acdaa1b081ed33351c01642ad7c9b8
ognestraz <ognestraz@gmail.com>
2016-08-28 17:23:53 +0300

    Fix auth by Laravel 5.3

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -1,15 +1,19 @@
 <?php namespace Ognestraz\Auth\Http\Controllers;
 
-use Illuminate\Foundation\Auth\ThrottlesLogins;
-use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends \Illuminate\Routing\Controller
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-    use AuthenticatesAndRegistersUsers, ThrottlesLogins;
+    use AuthenticatesUsers, RegistersUsers {
+        AuthenticatesUsers::redirectPath insteadof RegistersUsers;
+    }
 
     /**
      * Where to redirect users after login / registration.
@@ -18,7 +22,7 @@ class AuthController extends \Illuminate\Routing\Controller
      */
     protected $redirectTo = '/';
     protected $redirectAfterLogout = '/login';
-    
+
     protected $registerView = 'auth::register';
 
     /**
@@ -39,5 +43,42 @@ class AuthController extends \Illuminate\Routing\Controller
     public function showLoginForm()
     {
         return view('auth::login');
+    }
+
+    /**
+     * Show the application registration form.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function showRegistrationForm()
+    {
+        return view($this->registerView);
+    }
+
+    /**
+     * Get the guard to be used during authentication.
+     *
+     * @return \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected function guard()
+    {
+        return Auth::guard();
+    }
+
+    /**
+     * Log the user out of the application.
+     *
+     * @param  Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+
+        $request->session()->flush();
+
+        $request->session()->regenerate();
+
+        return redirect($this->redirectAfterLogout);
     }
 }
